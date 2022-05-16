6ea59890c4dbbd54aa0ec3ae6eeeef02644ead1e
Anushree Bera <anushreejsr@gmail.com>
2022-05-16 13:19:09 +0530

    Q2 and Q3

--- leetcode/lc-1.sql
+++ leetcode/lc-1.sql
@@ -1,6 +1,13 @@
 -- https://leetcode.com/discuss/general-discussion/1208129/list-of-free-leetcode-sql-questions
 
--- Q1 - https://leetcode.com/problems/swap-salary/submissions/
+-- Q1 - https://leetcode.com/problems/big-countries/
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+OR population >= 25000000;
+
+
+-- Q2 - https://leetcode.com/problems/swap-salary/submissions/
 UPDATE Salary  
 SET sex = CASE sex  
 WHEN 'm' THEN 'f'  
@@ -8,3 +15,11 @@ WHEN 'f' THEN 'm'
 ELSE NULL  
 END; 
 
+-- Q3 - https://leetcode.com/problems/not-boring-movies/
+SELECT *
+FROM Cinema
+WHERE id % 2 != 0
+AND description NOT LIKE '%boring%'
+ORDER BY rating DESC;
+
+-- Q4 - 
