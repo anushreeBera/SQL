857b882f70e7f718e035e80bcfbbff6f0d758221
Anushree Bera <anushreejsr@gmail.com>
2022-05-17 18:34:16 +0530

    Update lc-study-plan-easy.sql

--- leetcode/lc-study-plan-easy.sql
+++ leetcode/lc-study-plan-easy.sql
@@ -1,2 +1,5 @@
 -- Q - https://leetcode.com/problems/find-customer-referee/submissions/
 select name from Customer where referee_id != 2 OR referee_id IS NULL;
+
+-- Q - https://leetcode.com/problems/recyclable-and-low-fat-products/
+select product_id from products where low_fats = 'Y' and recyclable='Y';
