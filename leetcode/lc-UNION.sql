ec04c939d8381aa3e45e41527f111fcadd650398
Anushree Bera <anushreejsr@gmail.com>
2022-05-19 12:34:27 +0530

    Update and rename lc-union.sql to lc-UNION.sql

--- leetcode/lc-UNION.sql
+++ leetcode/lc-UNION.sql
@@ -4,4 +4,3 @@ UNION
 Select product_id,"store2" AS STORE ,store2 AS price FROM Products WHERE  store2 IS NOT NULL
 UNION
 Select product_id,"store3" AS STORE ,store3 AS price FROM Products WHERE  store3 IS NOT NULL;
-
