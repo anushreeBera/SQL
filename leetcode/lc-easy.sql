c37e2d70ea28ecaee0023a8d637ca7ed79c9bc29
Anushree Bera <anushreejsr@gmail.com>
2022-05-19 12:40:10 +0530

    Update lc-easy.sql

--- leetcode/lc-easy.sql
+++ leetcode/lc-easy.sql
@@ -178,3 +178,26 @@ FROM Salaries
 WHERE employee_id NOT IN (SELECT DISTINCT employee_id FROM Employees)
 ORDER By employee_id;
  -- NOTE : https://stackoverflow.com/questions/2384298/why-does-mysql-report-a-syntax-error-on-full-outer-join
+
+-- Q26 - https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+select event_day as day, emp_id, SUM(out_time - in_time) as total_time
+FROM Employees
+group by event_day, emp_id;
+
+-- Q27 - REFORMAT - https://leetcode.com/problems/reformat-department-table/
+SELECT 
+    id, 
+    SUM( IF( month = 'Jan', revenue, NULL ) ) AS Jan_Revenue,
+    SUM( IF( month = 'Feb', revenue, NULL ) ) AS Feb_Revenue,
+    SUM( IF( month = 'Mar', revenue, NULL ) ) AS Mar_Revenue,
+    SUM( IF( month = 'Apr', revenue, NULL ) ) AS Apr_Revenue,
+    SUM( IF( month = 'May', revenue, NULL ) ) AS May_Revenue,
+    SUM( IF( month = 'Jun', revenue, NULL ) ) AS Jun_Revenue,
+    SUM( IF( month = 'Jul', revenue, NULL ) ) AS Jul_Revenue,
+    SUM( IF( month = 'Aug', revenue, NULL ) ) AS Aug_Revenue,
+    SUM( IF( month = 'Sep', revenue, NULL ) ) AS Sep_Revenue,
+    SUM( IF( month = 'Oct', revenue, NULL ) ) AS Oct_Revenue,
+    SUM( IF( month = 'Nov', revenue, NULL ) ) AS Nov_Revenue,
+    SUM( IF( month = 'Dec', revenue, NULL ) ) AS Dec_Revenue
+FROM Department
+GROUP BY id;
