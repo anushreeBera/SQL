4bd19b98eb9ac3ffca0d6bf297c6356e01f1cabf
Anushree Bera <anushreejsr@gmail.com>
2022-05-29 22:44:07 +0530

    Update lc-medium.sql

--- leetcode/lc-medium.sql
+++ leetcode/lc-medium.sql
@@ -26,3 +26,9 @@ SELECT stock_name,
 SUM(CASE WHEN operation='Buy' THEN price ELSE 0 END)) AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name
+
+-- Q4 - https://leetcode.com/problems/rank-scores/
+SELECT score,
+       dense_rank() over (ORDER BY score DESC) 'rank'
+FROM Scores
+;
